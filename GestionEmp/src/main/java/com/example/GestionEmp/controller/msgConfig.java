a49355991d4a6460141d3b984d491e7803dbb502
salma <chakirisalma20@gmail.com>
2024-10-28 10:14:28 +0100

    internationalisation des message

--- GestionEmp/src/main/java/com/example/GestionEmp/controller/msgConfig.java
+++ GestionEmp/src/main/java/com/example/GestionEmp/controller/msgConfig.java
@@ -1,5 +1,17 @@
 package com.example.GestionEmp.controller;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+@Configuration
 public class msgConfig {
 
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages"); // Nom de la base de ressources (sans l'extension .properties)
+        messageSource.setDefaultEncoding("UTF-8"); // Encodage par défaut
+        return messageSource;
+    }
 }
